1f41c6ef73e1ab2aade61fca83ccf89c9e7e359c
suraj pattanayak <126487364+Suraj-kumar-pattnayak@users.noreply.github.com>
2025-05-21 13:00:08 +0530

    Update db.js

--- src/helper/db.js
+++ src/helper/db.js
@@ -1,13 +1,22 @@
 import mongoose from "mongoose";
 
+//states 0 ->disconnected, 1-> connected
+const config = {
+    isConnected: 0,
+}
 export async function connectDB(){
                         //asynchonous  -> synchronous    so async await
+    //if already connected => return
+    if(config.isConnected == 1) return;
+        
     try{                
        const {connection} = await mongoose.connect(process.env.MONGO_DB_URL, {   //imp
             dbName : "work_manager",
         });
 
         console.log("connection established suseccfully...");
+        console.log(connection.readyState);
+        config.isConnected = connection.readyState;
         // console.log("connection = ", connection);
 
         // console.log("working with this host ",connection.host);
