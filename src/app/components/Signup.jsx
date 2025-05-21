746f805beebf68ad319f0945f0ca8ec5cd69b286
suraj pattanayak <126487364+Suraj-kumar-pattnayak@users.noreply.github.com>
2025-05-21 15:45:49 +0530

    Update Signup.jsx

--- src/app/components/Signup.jsx
+++ src/app/components/Signup.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import {useState} from 'react';
 import Image from 'next/image';
-import signupsvg from '../../assets/signup.svg'
+import signupsvg from '../../assets/signup2.svg'
 import { signup } from '@/services/signupService';
 import { toast } from 'react-toastify';
 import { useRouter } from 'next/navigation';
